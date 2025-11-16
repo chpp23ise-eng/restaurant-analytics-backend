5322c3c92a358252e5ec55c54758d2e376b45711
chpp23ise-eng <chpp23ise@cmrit.ac.in>
2025-11-16 15:28:15 +0530

    final server update with CORS and production mode

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from fastapi import FastAPI
 from routes.analytics_routes import router as analytics_router
 from routes.order_routes import router as order_router
 from config.config import MODE
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Restaurant Analytics API")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all websites to access API
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def root():
